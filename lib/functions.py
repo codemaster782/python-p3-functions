319ca352b9cd5a54538d044abf48221dbac2724b
Julian katsivo <julian.katsivo@student.moringaschool.com>
2025-05-13 15:37:09 +0300

    added new changes

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    name= ("Guido")
+    print (f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print (f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    num1=45
+    num2=55
+    return num1+num2
     pass
 
 def halve(number):
+    num=2
+    return number/2
     pass
